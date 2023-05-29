1e7f219929344c9684aba3d74fa2cfac9faca4c8
Aysegul BAS <aysegul.bas@arkas.com>
2023-05-30 00:31:13 +0300

    tamam

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
 
@@ -6,30 +6,62 @@ import { data } from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
+  const list = JSON.parse(localStorage.getItem("basket"));
   const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(list ? list : []);
+
+  // useEffect(() => {
+  //   const list = JSON.parse(localStorage.getItem("basket"));
+  //   if (list) {
+  //     setCart(list);
+  //   }
+  // }, []);
 
   const addItem = (item) => {
-    // verilen itemi sepete ekleyin
+    const newCart = [...cart, item];
+    setCart(newCart);
+    console.log("yeni", newCart);
   };
 
+  const removeItem = (id) => {
+    // const filteredCart = cart.filter((f) => f.id !== id);
+    const tekSil = cart.find((item) => item.id === id);
+    let index = cart.indexOf(tekSil);
+    let newArr = [...cart];
+    newArr.splice(index, 1);
+    setCart(newArr);
+
+    // setCart(tekSil);
+    // console.log(index);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("basket", JSON.stringify(cart));
+  }, [cart]);
+
   return (
-    <div className="App">
-      <Navigation cart={cart} />
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={{ cart, removeItem }}>
+        <div className="App">
+          <Navigation />
 
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+          {/* Routelar */}
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
-    </div>
+            <Route path="/cart">
+              <ShoppingCart />
+            </Route>
+          </main>
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
   );
 }
 
---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,7 @@
+//Bu dosyada, react kitaplığından "createContext" işlevini import edin ve "ProductContext"imizi oluşturun.//
+import { createContext } from "react";
+export const ProductContext = createContext();
+// const ProductContext = ({ children }) => {
+//   return <ProductContext.Provider>{children}</ProductContext.Provider>;
+// };
+// export default ProductContext;
